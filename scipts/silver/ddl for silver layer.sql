1849bf21ac70923d65c57cdc89177a7ee3a29743
MuhammadFaizan420 <85739160+MuhammadFaizan420@users.noreply.github.com>
2025-05-21 12:45:56 +0500

    Update ddl for silver layer.sql

--- scipts/silver/ddl for silver layer.sql
+++ scipts/silver/ddl for silver layer.sql
@@ -1,1 +1,77 @@
+--// ddl for crm tables
+IF OBJECT_ID('silver.crm_cust_info','U') IS NOT NULL
+   DROP TABLE silver.crm_cust_info;
+CREATE TABLE silver.crm_cust_info
+(
+       cst_id int,
+	   cst_key varchar(50),
+	   cst_firstname varchar(50),
+	   cst_lastname varchar(50),
+	   cst_marital_status varchar(10),
+	   cst_gender varchar(10),
+	   cst_create_date Date,
+	   dwh_create_date Datetime2 Default getdate()
+);
+
+IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
+   DROP TABLE silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info
+(
+       prd_id int,
+	   cat_id varchar(50),
+	   prd_key varchar(50),
+	   prd_nm varchar(200),
+	   prd_cost int,
+	   prd_line varchar(50),
+	   prd_strt_dt Date,
+	   prd_end_dt Date,
+	   dwh_create_date Datetime2 Default getdate()
+);
+
+IF OBJECT_ID('silver.crm_sales_details','U') IS NOT NULL
+   DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details
+(
+       sls_ord_nm varchar(50),
+	   sls_prd_key varchar(50),
+	   sls_cust_id int,
+	   sls_ord_dt DATE,
+	   sls_ship_dt DATE,
+	   sls_due_dt DATE,
+	   sls_sales int,
+	   sls_quantity int,
+	   sls_price int,
+	   dwh_create_date Datetime2 Default getdate()
+);
+
+--// ddl for erp tables
+IF OBJECT_ID('silver.erp_loc_a101','U') IS NOT NULL
+   DROP TABLE silver.erp_loc_a101;
+CREATE TABLE silver.erp_loc_a101
+(
+        cid varchar(50),
+		cntry varchar(50),
+	   dwh_create_date Datetime2 Default getdate()
+);
+
+IF OBJECT_ID('silver.erp_cust_az12','U') IS NOT NULL
+   DROP TABLE silver.erp_cust_az12;
+CREATE TABLE silver.erp_cust_az12
+(
+        cid varchar(50),
+		bdate date,
+		gen varchar(50),
+	   dwh_create_date Datetime2 Default getdate()
+);
+
+IF OBJECT_ID('silver.erp_px_cat_g1v2','U') IS NOT NULL
+   DROP TABLE silver.erp_px_cat_g1v2;
+CREATE TABLE silver.erp_px_cat_g1v2
+(
+         id varchar(50),
+		 cat varchar(50),
+		 subcat varchar(50),
+		 maintenance varchar(50),
+	   dwh_create_date Datetime2 Default getdate()
+);
 
